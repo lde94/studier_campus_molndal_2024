48ec4fb25028146887496df8e904e4c67deb7fa9
lde94 <94ludeks@gafe.molndal.se>
2024-09-02 13:24:22 +0200

    Fortsatt med övningarna

--- oop_grundkurs/week1/FirstSegment/src/ExercisesAdv.java
+++ oop_grundkurs/week1/FirstSegment/src/ExercisesAdv.java
@@ -107,6 +107,142 @@ public class ExercisesAdv {
 
         //Uppgift 2.1
 
+        /*System.out.println("Please enter your amount of points: ");
+        Scanner scanner = new Scanner(System.in);
+        int points = scanner.nextInt();
+
+        if (points >= 90) {
+            System.out.println("You got an A grade!");
+        }
+        else if (points >= 80 && points < 90) {
+            System.out.println("You got an B grade!");
+        }
+        else if (points >= 70 && points < 80) {
+            System.out.println("You got an C grade!");
+        }
+        else if (points >= 60 && points < 70) {
+            System.out.println("You got an D grade!");
+        }
+        else if (points >= 50 && points < 60) {
+            System.out.println("You got an E grade!");
+        }
+        else if (points < 50) {
+            System.out.println("You got an F grade :(");
+        }
+        else {
+            System.out.println("Please insert the proper amount.");
+        }
+        scanner.close();*/
+
+        /*System.out.print("Please enter the year to check if it´s a leap year: ");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("It is a leap year.");
+        }
+        else {
+            System.out.println("It is not a leap year.");
+        }
+        scanner.close();*/
+
+
+        //Uppgift 2.2
+        /*System.out.print("Please enter the weekday you´d like to know your schedule for: ");
+        Scanner scanner = new Scanner(System.in);
+        String weekday = scanner.nextLine();
+
+        switch (weekday) {
+            case "Monday": {
+                System.out.println("Homework and grocery shopping");
+                break;
+            }
+            case "Tuesday": {
+                System.out.println("Lecture");
+                break;
+            }
+            case "Wednesday": {
+                System.out.println("Homework and gym");
+                break;
+            }
+            case "Thursday": {
+                System.out.println("Lecture");
+                break;
+            }
+            case "Friday": {
+                System.out.println("Lecture");
+                break;
+            }
+            case "Saturday": {
+                System.out.println("Gym");
+                break;
+            }
+            case "Sunday": {
+                System.out.println("Free time!");
+                break;
+            }
+            default: {
+                System.out.println("Invalid weekday");
+                break;
+            }
+        }
+        scanner.close();*/
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the first number: ");
+        double a = scanner.nextDouble();
+        System.out.print("Please enter the second number: ");
+        double b = scanner.nextDouble();
+        System.out.println("Now choose between the following: *, +, -, /");
+        String operator = scanner.next();
+
+        switch (operator) {
+            case "+": {
+                System.out.println(a + b);
+                break;
+            }
+            case "-": {
+                System.out.println(a - b);
+                break;
+            }
+            case "*": {
+                System.out.println(a * b);
+            }
+            case "/": {
+                System.out.println(a / b);
+                break;
+            }
+            default: {
+                System.out.println("Invalid operator");
+            }
+        }
+        scanner.close();*/
+
+        //Uppgift 3.1
+
+        /*for (int i = 100; i > 0; i--) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                System.out.println(i);
+            }
+
+        }*/
+
+        System.out.print("Please enter the faculty number: ");
+        Scanner scanner = new Scanner(System.in);
+        int facultyNumber = scanner.nextInt();
+
+        for (int i = 1; i < facultyNumber; i++) {
+
+            if(i > 0 && i % 2 == 0) {
+
+                System.out.println(i);
+            }
+
+        }
+        scanner.close();
+
+
+
 
 
     }
